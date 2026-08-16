c7848d9abb4c8b020226436b6532a3f8dd2b8742
angaityel <161797572+angaityel@users.noreply.github.com>
2026-08-16 17:35:55 +0300

    Test disable apex cloth

--- lua/scripts/game_state/state_ingame.lua
+++ lua/scripts/game_state/state_ingame.lua
@@ -190,21 +190,9 @@ function StateIngame:_setup_world()
 	local shading_callback = callback(self, "shading_callback")
 	local layer = 1
 	local shading_environment
-	local apex_disabled = Application.user_setting("disable_apex_cloth") ~= true or script_data.settings.dedicated_server
-	local has_apex = not not apex_disabled
-
-	local argv = {
-		Application.argv()
-	}
-	if table.find(argv, "-autohost") then
-		has_apex = false
-	end
-
-	if has_apex then
-		self.world = Managers.world:create_world(self.world_name, shading_environment, shading_callback, layer, Application.APEX_LOD_RESOURCE_BUDGET, Application.user_setting("apex_lod_resource_budget") or ApexClothQuality.high.apex_lod_resource_budget)
-	else
-		self.world = Managers.world:create_world(self.world_name, shading_environment, shading_callback, layer, Application.DISABLE_APEX_CLOTH)
-	end
+	local has_apex = false
+	
+	self.world = Managers.world:create_world(self.world_name, shading_environment, shading_callback, layer, Application.DISABLE_APEX_CLOTH)
 
 	World.set_data(self.world, "has_apex", has_apex)
 end
